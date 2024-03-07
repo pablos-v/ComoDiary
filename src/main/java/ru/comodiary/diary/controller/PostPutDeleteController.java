2d31c9f7886822eb2d7f39be593fa762dd2df799
Pablos <pablos@list.ru>
2024-03-07 19:55:04 +0300

    minor controller optimisations

--- src/main/java/ru/comodiary/diary/controller/PostPutDeleteController.java
+++ src/main/java/ru/comodiary/diary/controller/PostPutDeleteController.java
@@ -8,35 +8,30 @@ import org.springframework.web.servlet.ModelAndView;
 import ru.comodiary.diary.model.Task;
 import ru.comodiary.diary.service.TaskService;
 
-import java.util.Objects;
-
 @Controller
 @AllArgsConstructor
 public class PostPutDeleteController {
 
     private final TaskService service;
 
+    // создать новую
     @PostMapping("/task")
-    public String addNewTask(@RequestParam("title") String title,
-                             @RequestParam("description") String description,
-                             @RequestParam("expireDate") String expireDate,
-                             @RequestParam("status") String status,
-                             Model model) {
+    public ModelAndView addNewTask(@RequestParam("title") String title,
+                                   @RequestParam("description") String description,
+                                   @RequestParam("expireDate") String expireDate,
+                                   @RequestParam("status") String status) {
         Task task = new Task(title, description, expireDate, status);
         service.addOrUpdateTask(task);
-        model.addAttribute("expired", service.updateAndGetAllExpiredTasks());
-        model.addAttribute("day", service.getAllDayTasks(expireDate));
-        model.addAttribute("dayName", service.getDayName(expireDate));
-        return "day";
+
+        return new ModelAndView("redirect:" + "/day?date=" + task.getExpireDate());
     }
 
     // фактически это PUT, но HTML формы поддерживают только 2 метода GET и POST... ссыль в литературе есть
     @PostMapping("/task/{id}")
-    public String updateTask(@PathVariable Long id, @RequestParam("title") String title,
-                             @RequestParam("description") String description,
-                             @RequestParam("expireDate") String expireDate,
-                             @RequestParam(name = "status", defaultValue = "no") String status,
-                             Model model) {
+    public ModelAndView updateTask(@PathVariable Long id, @RequestParam("title") String title,
+                                   @RequestParam("description") String description,
+                                   @RequestParam("expireDate") String expireDate,
+                                   @RequestParam(name = "status", defaultValue = "no") String status) {
         Task task = service.getTaskById(id);
         task.setTitle(title);
         task.setDescription(description);
@@ -45,34 +40,18 @@ public class PostPutDeleteController {
 
         service.addOrUpdateTask(task);
 
-        model.addAttribute("expired", service.updateAndGetAllExpiredTasks());
-        model.addAttribute("day", service.getAllDayTasks(expireDate));
-        model.addAttribute("dayName", service.getDayName(expireDate));
-        return "day";
+        return new ModelAndView("redirect:" + "/day?date=" + task.getExpireDate());
     }
 
     // удаляет задачу и редиректит обратно в день
     @PostMapping("/delete/{id}")
-    public ModelAndView deleteById(@PathVariable Long id, Model model) {
+    public ModelAndView deleteById(@PathVariable Long id) {
         Task task = service.deleteTaskById(id);
-        model.addAttribute("day", service.getAllDayTasks(task.getExpireDate()));
+
         return new ModelAndView("redirect:" + "/day?date=" + task.getExpireDate());
     }
-// унифицировать и получать ИД в @RequestParam ???
-//    @PostMapping("/change-status/go-to-day/{id}")
-//    public ModelAndView changeStatusByIdAndGoToDayView(@PathVariable Long id, Model model) {
-//        Task task = service.changeStatus(id);
-//        model.addAttribute("day", service.getAllDayTasks(task.getExpireDate().toString()));
-//        return new ModelAndView("redirect:" + "/day?date=" + task.getExpireDate());
-//    }
-//
-//    @PostMapping("/change-status/go-to-list/{id}")
-//    public ModelAndView changeStatusByIdAndGoToListView(@PathVariable Long id, Model model) {
-//        Task task = service.changeStatus(id);
-//        model.addAttribute("day", service.getAllDayTasks(task.getExpireDate().toString()));
-//        return new ModelAndView("redirect:" + "/list?search= ТУТ ПАРАМЕТР из @RequestParam" );
-//    }
 
+    // смена статуса задачи и редирект смотря куда надо
     @PostMapping("/change-status")
     public ModelAndView changeTaskStatus(@RequestParam("id") String id,
                                          @RequestParam("whereTo") String whereTo) {
